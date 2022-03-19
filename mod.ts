1672ae47f1dc8dc581fc758b371eaa58014f4a2f
Jim Burbridge <jhechtf@gmail.com>
2022-03-19 14:39:38 -0700

    feat: Adds in Config type
    Makes some modifications to Token type to allow number values to be passed.
    
    Closes #5

--- mod.ts
+++ mod.ts
@@ -2,3 +2,4 @@ export { default as Token } from './src/token.ts';
 export { default as MediaQuery } from './src/media.ts';
 export { default as Stylesheet } from './src/stylesheet.ts';
 export { default as Selector } from './src/selector.ts';
+export type { Config } from './src/types.d.ts';
