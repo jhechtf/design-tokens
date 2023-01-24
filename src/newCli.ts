1c1497da97c9937179701cc184e14602050660fc
Jim Burbridge <jhechtf@gmail.com>
2023-01-23 22:34:08 -0800

    chore: testing remote run

--- src/newCli.ts
+++ src/newCli.ts
@@ -2,15 +2,10 @@ import Cli from './cli/cli.ts';
 import { container } from '../deps.ts';
 
 const COMMAND_DIR = new URL('./cli/commands', import.meta.url);
-// const base = fromFileUrl(COMMAND_DIR);
+const commands = ['main.ts'];
 
-for await (const entry of Deno.readDir(COMMAND_DIR)) {
-  console.info(entry);
-  if (entry.isFile) {
-    const filePath = new URL('./commands/'+entry.name, COMMAND_DIR);
-    console.info(filePath);
-    await import(filePath.href);
-  }
+for (const fileName of commands) {
+  await import(new URL('./commands/' + fileName, COMMAND_DIR).href);
 }
 
 const cli = container.resolve(Cli);
