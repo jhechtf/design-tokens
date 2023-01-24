bb5db45054cc18e09329d16951b0d5a62dcf62aa
Jim Burbridge <jhechtf+git@gmail.com>
2023-01-23 18:39:01 -0800

    wip: testing

--- src/newCli.ts
+++ src/newCli.ts
@@ -1,15 +1,18 @@
 import Cli from './cli/cli.ts';
-import { container, fromFileUrl, toFileUrl, join } from '../deps.ts';
+import { container } from '../deps.ts';
 
 const COMMAND_DIR = new URL('./cli/commands', import.meta.url);
-const base = fromFileUrl(COMMAND_DIR);
+// const base = fromFileUrl(COMMAND_DIR);
 
 for await (const entry of Deno.readDir(COMMAND_DIR)) {
+  console.info(entry);
   if (entry.isFile) {
-    const filePath = toFileUrl(join(base, entry.name));
+    const filePath = new URL('./commands/'+entry.name, COMMAND_DIR);
+    console.info(filePath);
     await import(filePath.href);
   }
 }
 
-const thing = container.resolve(Cli);
-console.info(thing);
+const cli = container.resolve(Cli);
+
+console.info(cli);
